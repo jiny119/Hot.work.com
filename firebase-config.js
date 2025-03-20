d57b44c9f6a051cba71fb905788bec2bb9a6fa91
jiny119 <nawab5853656@gmail.com>
2025-03-21 02:08:18 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,21 @@
-// Firebase Configuration
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAqKbPSwF_INJfGD7ADuo5-YkzO9dpJAL4",
-    authDomain: "mytaskapp-e0e42.firebaseapp.com",
-    databaseURL: "https://mytaskapp-e0e42-default-rtdb.firebaseio.com",
-    projectId: "mytaskapp-e0e42",
-    storageBucket: "mytaskapp-e0e42.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, 
+  authDomain: "mytaskapp-e0e42.firebaseapp.com",
+  databaseURL: "https://mytaskapp-e0e42-default-rtdb.firebaseio.com",
+  projectId: "mytaskapp-e0e42",
+  storageBucket: "mytaskapp-e0e42.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID, 
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 const database = firebase.database();
 
-// Export Firebase Modules
 export { auth, database };
